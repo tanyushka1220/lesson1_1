e4cad6137b9eded70cb92083d87f0404ae7c517e
Tanya <tanyafi2010@gmail.com>
2023-10-01 13:35:25 +0300

    class students

--- main.py
+++ main.py
@@ -1,2 +1,45 @@
-start
-print("Hello Me")
+class Student:
+    def __init__(self, name, surname, age, logiks,
+                 group, login,password, tg):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.logiks = logiks
+        self.tg = tg
+        self.login = login
+        self.password = password
+        self.group = group
+
+    def all_info(self):
+        print("Вся інформація студентів")
+        print("Імя" - , self.name)
+        print("" - , self.surname)
+        print("Імя" - , self.age)
+        print("Імя" - , self.logiks)
+        print("Імя" - , self.group)
+        print("Імя" - , self.login)
+        print("Імя" - , self.password)
+        print("Імя" - , self.tg)
+    
+    def logiks_info(self):
+        print("Логіки")
+        lg = 
+        for logik in self.logiks:
+            lg += str(logik) + ","
+        all_lg = sum(self.logiks)
+        print(lg)
+        print("All logiks:", all_lg)
+        print("")
+
+
+students = list()
+Student = Student("Tanya", "Filipova", 14, [5,5,5,5], "Python", )
+students.append(student)
+for i in range:
+    print(i,"", students[i].surname)
+    who = int(input("?"))
+    if do == 1:
+        
+while True:
+    print("///")
+    do = int(input("?"))
